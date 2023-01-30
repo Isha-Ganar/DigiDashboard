5c63f7f2db487708f779637bf70f931fef58a2a7
Isha-Ganar <82870462+Isha-Ganar@users.noreply.github.com>
2023-01-30 21:50:05 +0530

    Classes Objects Methods

--- Animal.java
+++ Animal.java
@@ -1,34 +1,79 @@
-package com.ishaganar.LearnConstructor;
-
-//Constructor Practice 
+package com.ishaganar.LearnObjectsClassesMethods;
 
 
-public class Animal 
+//Class
+//if Access modifier is not specified before class then "Default" is applied.
+public class Animal
 {
-	String Name;
-	int Age;
+	//Attributes //variables 
+	String colour;
+	int age;
 	
-	//Default Constructor is created by Compiler
 	
-	//non Parameterized 
-	//User Defined Non argument Constructor 
-	Animal()
-	{
-		
-	}
 	
-	// Parameterized Constructor
-	Animal(String N, int A)
-	{
-		this.Name = N;
-		this.Age = A;
-	}
-	
-	public static void main(String[] args) 
-	{
-		Animal an = new Animal("tommy" , 2); // initialize object by using Constructor
-		System.out.println(an.Name);
-		System.out.println(an.Age);
+	public Animal(String colour, int age) {
+		super();
+		this.colour = colour;
+		this.age = age;
 	}
 
+	public Animal() {
+	
+	}
+
+	//Method
+	public void eat()
+	{
+		System.out.println("I Am eating");
+	}
+	
+	//Method to initialize object 
+	void InitObj(String c, int a)
+	{
+		colour = c ;
+		age = a ;
+	}
+	
+	void Display() 
+	{
+		System.out.println(colour +" "+ age );
+	}
+	
+	//Main Method
+	public static void main(String[] args)
+	{
+		//Statements will run in Top Down Sequence.
+		Animal Buzo = new Animal();  // Object Creation
+		Buzo.eat(); // Calling the method
+		Buzo.run();
+		
+		
+		///--Initializing the Object--///
+		
+		//1) ---By Using Reference Variable---//
+			Buzo.age = 10 ;
+			Buzo.colour = "Brown" ;
+			System.out.println("Age " + Buzo.age + " ===> colour " + Buzo.colour);
+		
+		//2) ---By using Method---//
+		    Buzo.InitObj("Pink", 7);
+		    Buzo.Display();
+		    
+		//3) ---By using Constructor---//
+		    Animal Buzo2 = new Animal("Tommy",6);
+		    System.out.println();
+		    Buzo2.Display();
+		    
+	}
+
+	//method
+	public void run()
+	{
+		System.out.println("I am running");
+	}
+	
+	
+	
+	
+	
 }
